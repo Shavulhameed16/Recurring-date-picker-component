331da0d4dd39859dcfabb1bc59617605fceaafff
Shavul27 <shavulhameed085@gmail.com>
2025-07-19 11:36:07 +0530

    Create CalenderPreview.js

---
+++ CalenderPreview.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import useStore from './store';
+const CalendarPreview = () => {
+  const { startDate, endDate, recurrenceType } = useStore();
+  
+  return (
+    <div>
+      <h2>Calendar Preview</h2>
+      {}
+    </div>
+  );
